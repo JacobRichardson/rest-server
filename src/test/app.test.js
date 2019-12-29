39208ec3eef34aaf37f6065a0421a968582275dc
Jacob Richardson <44219889+JacobRichardson@users.noreply.github.com>
2019-12-29 12:25:22 -0500

    Create src/test/app.test.js.
    This commit creates the test file for the app module. This test
    file creates a complie test to make sure the app module complies.
    More tests need to be written.

---
+++ src/test/app.test.js
@@ -0,0 +1,27 @@
+/** 
+ * This file tests the app file.
+ */
+
+// Imports.
+const test = require('tape');
+
+// Test to see if app complies.
+test('app complies.', t => {
+
+	try {
+
+		// Require in the app module.
+		require('../app');
+
+		// Pass the test.
+		t.pass('No error requiring in the app module.');
+	}
+	catch (e) {
+
+		// Fail the test with the error.
+		t.fail(e);
+	}
+
+	// End the test.
+	t.end();
+});
