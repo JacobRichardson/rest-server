aa8045c51931febf421f93489b7f7870092790a8
Jacob Richardson <44219889+JacobRichardson@users.noreply.github.com>
2019-12-29 20:34:15 -0500

    Updating src/app - routes, db, body parser, etc.
    This commit updates the app file to include the transaction routes
    and the db into the app. This commit also has the express app to
    use body parser as json.

--- src/app.js
+++ src/app.js
@@ -3,10 +3,22 @@
  */
 
 // Imports.
+require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('./lib/db');
+const transactionRoutes = require('./routes/transaction');
 
 // Globals.
 const app = express();
 
+app.use(bodyParser.json());
+
+// Use the transaction routes on the /transaction url.
+app.use('/transactions', transactionRoutes);
+
 // Export the app.
 module.exports = app;
+
+// Export the close function from the db.
+module.exports.close = db.close;
