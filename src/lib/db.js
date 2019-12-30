e926f873b27effac972009d2b72deee580485710
Jacob Richardson <44219889+JacobRichardson@users.noreply.github.com>
2019-12-29 20:23:54 -0500

    Creates src/lib/ folder.
    This commit creates the src/lib/ folder that will hold any
    library modules necessary for the project. This commit also
    creates the db module which connects to the mongo database.

---
+++ src/lib/db.js
@@ -0,0 +1,26 @@
+/** 
+ * This module is responsible for connecting
+ * to the database.
+ */
+
+// Imports.
+const config = require('../config/config');
+const mongoose = require('mongoose');
+
+// Make mongo use promises.
+mongoose.Promise = Promise;
+
+// Set debug to true.
+mongoose.set('debug', true);
+
+// Set connection string equal to the connection string in env or the config connection string the .
+const connectString = process.env.DATABASE_CONNECTION_STRING || config.database.connectionString;
+
+// Connect to the database with the connection string.
+mongoose.connect(connectString);
+
+// Export the the transaction model.
+module.exports.transaction = require('../models/transaction');
+
+// Export a close function.
+module.exports.close = () => mongoose.connection.close();
