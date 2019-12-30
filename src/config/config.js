43ec3089d21ac346bbd4d95894de5317aba2617a
Jacob Richardson <44219889+JacobRichardson@users.noreply.github.com>
2019-12-29 20:28:53 -0500

    Creates /src/config folder.
    This commit creates the config folder. Any configuration
    infomration for the project will go into this folder. This commit
    also creates the config file in this folder which is going to be
    the main config object.

---
+++ src/config/config.js
@@ -0,0 +1,15 @@
+/** 
+ * This is the config file. This file will 
+ * contain all configurable values.
+ */
+
+// The config object.
+const config = {
+
+	database: {
+		connectionString: ''
+	}
+};
+
+// Export the config object.
+module.exports = config;
